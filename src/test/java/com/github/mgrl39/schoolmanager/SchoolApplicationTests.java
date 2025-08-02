12f6bd891677c93e023c45935e093ea5facf1c71
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-08-02 07:38:53 +0200

    add base project

---
+++ src/test/java/com/github/mgrl39/schoolmanager/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mgrl39.schoolmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
