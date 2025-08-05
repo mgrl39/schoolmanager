b07bd05aba28d0c685fe14f78ec9e7ab5afa5242
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-08-05 22:06:02 +0200

    generated file

--- srcs/front/.svelte-kit/generated/server/internal.js
+++ srcs/front/.svelte-kit/generated/server/internal.js
@@ -21,7 +21,7 @@ export const options = {
 		app: ({ head, body, assets, nonce, env }) => "<!doctype html>\n<html lang=\"en\">\n\t<head>\n\t\t<meta charset=\"utf-8\" />\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n\t\t" + head + "\n\t</head>\n\t<body data-sveltekit-preload-data=\"hover\">\n\t\t<div style=\"display: contents\">" + body + "</div>\n\t</body>\n</html>\n",
 		error: ({ status, message }) => "<!DOCTYPE html>\n<html lang=\"en\">\n\t<head>\n\t\t<meta charset=\"utf-8\" />\n\t\t<title>" + message + "</title>\n\t</head>\n\t<body>\n\t\t<h1>My custom error page</h1>\n\t\t<p>Status: " + status + "</p>\n\t\t<p>Message: " + message + "</p>\n\t</body>\n</html>\n<!-- no sale no -->"
 	},
-	version_hash: "l261zu"
+	version_hash: "17hdkcz"
 };
 
 export async function get_hooks() {
