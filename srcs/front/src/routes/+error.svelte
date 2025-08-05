61a8346292e9b767844003fa5de0b3defb1c9291
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-08-05 22:01:47 +0200

    add error things

---
+++ srcs/front/src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let error: Error & {status?: number};
+</script>
+<main class="container">
+  <h1>{error?.message ?? 'ERROR'}</h1>
+  <a href="/">Go to home</a>
+</main>
+<!-- no sale tt -->
